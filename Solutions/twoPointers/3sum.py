9565271300fdd26c668ecdc5bef12df944c8345e
Alfred Bartholomew Sunday <alfred.bartholomewsunday@gmail.com>
2026-05-05 13:29:30 +0000

    3 Sum

---
+++ Solutions/twoPointers/3sum.py
@@ -0,0 +1,48 @@
+'''
+def threeSum(nums):
+    result = set()
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            for k in range(j + 1, len(nums)):
+                if nums[i] + nums[j] + nums[k] == 0:
+                    triplet = tuple(sorted([nums[i], nums[j], nums[k]]))
+                    result.add(triplet)
+
+    return [list(t) for t in result]
+                
+nums = [-1,0,1,2,-1,-4]
+res = threeSum(nums)
+print(res)
+
+'''
+class Solution(object):
+    def threeSum(self, nums):
+        nums.sort()
+        res_lst = []
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    res_lst.append([nums[i], nums[left], nums[right]])
+
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+        return res_lst
